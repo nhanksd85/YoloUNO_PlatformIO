8a0ee78bafda31a942f95f1d0fa46e97f843d12c
MSI\trong <nhanksd85@gmail.com>
2026-09-15 23:33:18 +0700

    Init example source code for LED and NEO

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,51 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-void TaskLEDControl(void *pvParameters) {
-  pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
-  int ledState = 0;
-  while(1) {
-    
-    if (ledState == 0) {
-      digitalWrite(GPIO_NUM_48, HIGH); // Turn ON LED
-    } else {
-      digitalWrite(GPIO_NUM_48, LOW); // Turn OFF LED
+#define NEO_PIN 45
+#define LED_COUNT 1 
+
+
+void neo_blinky(void *pvParameters){
+
+    Adafruit_NeoPixel strip(LED_COUNT, NEO_PIN, NEO_GRB + NEO_KHZ800);
+    strip.begin();
+    // Set all pixels to off to start
+    strip.clear();
+    strip.show();
+
+    while(1) {                          
+        strip.setPixelColor(0, strip.Color(255, 0, 0)); // Set pixel 0 to red
+        strip.show(); // Update the strip
+
+        // Wait for 500 milliseconds
+        vTaskDelay(500);
+
+        // Set the pixel to off
+        strip.setPixelColor(0, strip.Color(0, 0, 0)); // Turn pixel 0 off
+        strip.show(); // Update the strip
+
+        // Wait for another 500 milliseconds
+        vTaskDelay(500);
     }
-    ledState = 1 - ledState;
-    vTaskDelay(2000);
+}
+
+void led_blinky(void *pvParameters) {
+  pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
+
+  while(1) {
+    digitalWrite(GPIO_NUM_48, HIGH); // Turn ON LED
+    vTaskDelay(1000);
+    digitalWrite(GPIO_NUM_48, LOW); // Turn OFF LED
+    vTaskDelay(1000);
   }
 }
 
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once for task creation
   Serial.begin(115200);
-  xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
+  xTaskCreate(led_blinky, "LED Control", 2048, NULL, 2, NULL);
+  xTaskCreate(neo_blinky, "NEO Control", 2048, NULL, 2, NULL);
 }
 
 void loop() {
